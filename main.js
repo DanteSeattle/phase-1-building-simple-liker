8a2c8827e7e78038b7ca40b867310264d9ec00f0
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-04-14 11:26:06 -0700

    commit tree 2f5be5d96a50e3284ff4aabcc5a86883709a9dfd

--- main.js
+++ main.js
@@ -4,8 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Hide error on page load
+document.getElementById("modal").classList.add("hidden")
+document.addEventListener("DOMContentLoaded", () =>  {
+  document.getElementById("modal").classList.add("hidden")
 
+})
 
+// Add event listener to heart
+const heart = document.getElementsByClassName('like-glyph')
+for(var i = 0; i < heart.length; i++){
+  heart[i].addEventListener('click', heartClick)
+}
+
+//heart.forEach(item => item.addEventListener('click', heartClick))
+
+// Handle heart click event
+function heartClick(e){
+  console.log("clicked!")
+  mimicServerCall()
+    .then( () => {
+      console.log("success")
+      if (this.classList.contains('activated-heart'))
+        this.classList.remove('activated-heart')
+      else
+        this.classList.add('activated-heart')
+    })
+    // On return error display error modal and then remove after 3 sec
+    .catch( () => {
+      console.log("error")
+      document.getElementById("modal").classList.remove("hidden")
+      setTimeout(() => { document.getElementById("modal").classList.add("hidden") }, 3000)
+    })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
